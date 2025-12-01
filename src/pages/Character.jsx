35b82269715585ce9cc2a044481a7c239251228b
Alejandro Martín Tavira <95539127+Asiiz127@users.noreply.github.com>
2025-12-01 21:51:05 +0100

    routes changes

---
+++ src/pages/Character.jsx
@@ -0,0 +1,8 @@
+export const Character = () => {
+    return (
+        <div>
+            <h1>Character Page</h1>
+            <p>This is the character page content.</p>
+        </div>
+    );
+};
